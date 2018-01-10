6c63297f221bca0fd3a0330af876bb22bcc49cde
Nicolas "Norswap" Laurent <norswap@gmail.com>
2018-01-10 16:19:43 +0100

    add auto-casts to tree accessors, ass well as a wildcard for index chains

--- src/norswap/skelex/MatchTree.java
+++ src/norswap/skelex/MatchTree.java
@@ -3,6 +3,7 @@ package norswap.skelex;
 import norswap.skelex.regex.*;
 import norswap.utils.ArrayStack;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
@@ -25,7 +26,21 @@ import static norswap.utils.Util.cast;
  */
 public final class MatchTree extends Match
 {
-    // ---------------------------------------------------------------------------------------------
+    // =============================================================================================
+
+    /**
+     * A wildcard that can be used within chained index functions {@link #get(Object, int...)}.
+     * <p>
+     * Whenever the wildcard is used instead of a regular index, the result of the access is a list
+     * where each item is the result of the rest of the access chain.
+     * <p>
+     * For instance, {@code tree.get($, 2)} returns a list of items at index 2 within the sub-lists
+     * held within the root list. It's equivalent to the pseudo-code {@code tree.list().foreach {
+     * get(it, 2) }}.
+     */
+    public static final int $ = -42;
+
+    // =============================================================================================
 
     private static final Object MARKER = new Object();
 
@@ -100,13 +115,23 @@ public final class MatchTree extends Match
         list.add(top);
     }
 
-    // ---------------------------------------------------------------------------------------------
+    // =============================================================================================
+    // ROOT ACCESS
 
     /**
      * Returns the root of the matched tree.
      */
-    public Object value() {
+    public Object $value() {
         return stack.peek();
+    }
+
+    // ---------------------------------------------------------------------------------------------
+
+    /**
+     * Returns the root of the matched tree, attempting to cast it to the expected type.
+     */
+    public <T> T value() {
+        return cast(stack.peek());
     }
 
     // ---------------------------------------------------------------------------------------------
@@ -127,7 +152,8 @@ public final class MatchTree extends Match
         return cast(stack.peek());
     }
 
-    // ---------------------------------------------------------------------------------------------
+    // =============================================================================================
+    // STATIC CHAIN ACCESS
 
     /**
      * Returns the object obtained by casting {@code obj} into a list, then getting the item
@@ -137,19 +163,31 @@ public final class MatchTree extends Match
      * If at some point an item cannot be cast into a list or a list is too short for the given
      * index, an exception is thrown.
      */
-    public static Object get (Object obj, int... indices)
+    public static Object $get (Object obj, int... indices)
     {
         int i = 0;
         while (i < indices.length)
         {
             int index = indices[i];
 
-            if (obj instanceof Branch) {
+            if (obj instanceof Branch)
+            {
                 obj = ((Branch) obj).value;
                 continue;
             }
             else if (obj instanceof List)
-                obj = ((List<?>) obj).get(index);
+            {
+                List<?> list = (List<?>) obj;
+
+                if (index == $) {
+                    ArrayList<Object> out = new ArrayList<>(list.size());
+                    for (Object o: list)
+                        out.add($get(o, Arrays.copyOfRange(indices, i + 1, indices.length)));
+                    return out;
+                }
+
+                obj = list.get(index);
+            }
             else
                 throw new IllegalArgumentException(i + "th item in the path cannot be indexed");
 
@@ -161,16 +199,25 @@ public final class MatchTree extends Match
     // ---------------------------------------------------------------------------------------------
 
     /**
+     * Like {@link #$get}, but attempting to cast the result to the required type.
+     */
+    public static <T> T get(Object obj, int... indices) {
+        return cast($get(obj, indices));
+    }
+
+    // ---------------------------------------------------------------------------------------------
+
+    /**
      * Returns the object obtained by casting {@code obj} into a list, then getting the item
      * at the first index in {@code indices}, then repeatedly applying this process on the last
      * obtained item until all indices have been exhausted.
      * <p>
      * If at some point the function attempts to index a null value, or if a list is too short
-     * for the given index, null is returned. This is how this function differs from {@link #get}.
+     * for the given index, null is returned. This is how this function differs from {@link #$get}.
      * <p>
      * If at some point an item cannot be cast into a list, an exception is thrown.
      */
-    public static Object fmap (Object obj, int... indices)
+    public static Object $fmap (Object obj, int... indices)
     {
         int i = 0;
         while (i < indices.length)
@@ -185,13 +232,19 @@ public final class MatchTree extends Match
             {
                 List<?> list = ((List<?>) obj);
 
-                if (list.size() <= index) // ADDITION to get() code
+                if (index == $) {
+                    ArrayList<Object> out = new ArrayList<>(list.size());
+                    for (Object o: list)
+                        out.add($fmap(o, Arrays.copyOfRange(indices, i + 1, indices.length)));
+                    return out;
+                }
+
+                if (list.size() <= index) // ADDITION to $get() code
                     return null;
 
                 obj = list.get(index);
             }
-
-            else if (obj == null) // ADDITION to get() code
+            else if (obj == null) // ADDITION to $get() code
                 return null;
             else
                 throw new IllegalArgumentException("item " + i + " in the path cannot be indexed");
@@ -204,37 +257,56 @@ public final class MatchTree extends Match
     // ---------------------------------------------------------------------------------------------
 
     /**
-     * Same as {@link #get} but attempts to cast the result to a list.
+     * Like {@link #$fmap}, but attempting to cast the result to the required type.
+     */
+    public static <T> T fmap (Object obj, int... indices) {
+        return cast($fmap(obj, indices));
+    }
+
+    // ---------------------------------------------------------------------------------------------
+
+    /**
+     * Same as {@link #$get} but attempts to cast the result to a list.
      */
     public static List<?> get_list (Object obj, int... indices) {
-        return cast(get(obj, indices));
+        return cast($get(obj, indices));
     }
 
     // ---------------------------------------------------------------------------------------------
 
     /**
-     * Same as {@link #fmap}, but attempts to cast the result to a list.
+     * Same as {@link #$fmap}, but attempts to cast the result to a list.
      */
     public static List<?> fmap_list (Object obj, int... indices) {
-        return cast(fmap(obj, indices));
+        return cast($fmap(obj, indices));
     }
 
     // ---------------------------------------------------------------------------------------------
 
     /**
-     * Same as {@link #get}, but attempts to cast the result to a {@link Branch}.
+     * Same as {@link #$get}, but attempts to cast the result to a {@link Branch}.
      */
     public static Branch get_branch (Object obj, int... indices) {
-        return cast(get(obj, indices));
+        return cast($get(obj, indices));
     }
 
     // ---------------------------------------------------------------------------------------------
 
     /**
-     * Same as {@link #get}, but attempts to cast the result to a {@link Branch}.
+     * Same as {@link #$get}, but attempts to cast the result to a {@link Branch}.
      */
     public static Branch fmap_branch (Object obj, int... indices) {
-        return cast(fmap(obj, indices));
+        return cast($fmap(obj, indices));
+    }
+
+    // =============================================================================================
+    // CHAIN ACCESS FROM ROOT
+
+    /**
+     * Same as {@link #$get(Object, int...)}, using the root of the tree as first parameter.
+     */
+    public Object $get (int... indices) {
+        return $get(this, indices);
     }
 
     // ---------------------------------------------------------------------------------------------
@@ -242,8 +314,17 @@ public final class MatchTree extends Match
     /**
      * Same as {@link #get(Object, int...)}, using the root of the tree as first parameter.
      */
-    public Object get (int... indices) {
+    public <T> T get (int... indices) {
         return get(this, indices);
+    }
+
+    // ---------------------------------------------------------------------------------------------
+
+    /**
+     * Same as {@link #$fmap(Object, int...)}, using the root of the tree as first parameter.
+     */
+    public Object $fmap (int... indices) {
+        return $fmap(this, indices);
     }
 
     // ---------------------------------------------------------------------------------------------
@@ -251,9 +332,10 @@ public final class MatchTree extends Match
     /**
      * Same as {@link #fmap(Object, int...)}, using the root of the tree as first parameter.
      */
-    public Object fmap (int... indices) {
+    public <T> T fmap (int... indices) {
         return fmap(this, indices);
     }
+
     // ---------------------------------------------------------------------------------------------
 
     /**
@@ -271,6 +353,7 @@ public final class MatchTree extends Match
     public List<?> fmap_list (int... indices) {
         return fmap_list(this, indices);
     }
+
     // ---------------------------------------------------------------------------------------------
 
     /**
